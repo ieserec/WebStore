42ad467260d18198ef38849f80b2f2e8092969cb
JENNIFER SAMANTA MINAYO YUJATO <ieserec.solutions@gmail.com>
2020-12-18 19:08:34 -0500

    Commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Archivo CSS de
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'; // Archivo Javascript de Bootstrap 4 
  
 
+firebase.initializeApp({
+  apiKey: "AIzaSyATTijYL4HnfwNvN-geedULKrwZXV7TZR0",
+  authDomain: "ieserec.firebaseapp.com",
+  projectId: "ieserec",
+  storageBucket: "ieserec.appspot.com",
+  messagingSenderId: "398807098345",
+  appId: "1:398807098345:web:1360eb43de7ecc8371e178",
+  measurementId: "G-T29RX82ENR"
+
+  
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
